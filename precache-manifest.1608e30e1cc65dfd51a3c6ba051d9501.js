0fcdf3078db17d0bedc6955f12d14ba2bbd029cb
jemonroyb <c19315@utp.edu.pe>
2022-06-12 20:40:18 -0500

    carga

---
+++ precache-manifest.1608e30e1cc65dfd51a3c6ba051d9501.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dd87a5b61feb26f65f604ba025a7a26",
+    "url": "/jemonroyb/UTPSeguridad/index.html"
+  },
+  {
+    "revision": "4bad9226079d157eff38",
+    "url": "/jemonroyb/UTPSeguridad/static/css/2.feed411e.chunk.css"
+  },
+  {
+    "revision": "9202396cab560ef467db",
+    "url": "/jemonroyb/UTPSeguridad/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4bad9226079d157eff38",
+    "url": "/jemonroyb/UTPSeguridad/static/js/2.ec20dd56.chunk.js"
+  },
+  {
+    "revision": "dbbcc8ad780af68a63f1d6e5c831a121",
+    "url": "/jemonroyb/UTPSeguridad/static/js/2.ec20dd56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9202396cab560ef467db",
+    "url": "/jemonroyb/UTPSeguridad/static/js/main.43748c3a.chunk.js"
+  },
+  {
+    "revision": "819d9cda34f0c8c60862",
+    "url": "/jemonroyb/UTPSeguridad/static/js/runtime-main.79fcc2cc.js"
+  },
+  {
+    "revision": "5cc6ceb5cfcd284f42412e09508c7c66",
+    "url": "/jemonroyb/UTPSeguridad/static/media/walksafeHome.5cc6ceb5.png"
+  },
+  {
+    "revision": "2bc7fb4a008d31764cd65fb8e63e8536",
+    "url": "/jemonroyb/UTPSeguridad/static/media/walksafeSettings.2bc7fb4a.png"
+  }
+]);
